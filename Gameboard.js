0cdb5ca6a31de823a7d8e88e4c28cb5e8b0ccaa4
lukas paul <lukas@172.31.172.200>
2024-08-08 11:42:06 +0200

    draft of placeShip function

--- Gameboard.js
+++ Gameboard.js
@@ -23,4 +23,43 @@ class Gameboard {
             console.log('Miss');
         }
     }
+
+    placeShip(x, y, length, direction) {
+        let hasPlace = true;
+        if (direction === 'horizontal') {
+            if (y + length > 10) {
+                hasPlace = false;
+            } else {
+                for (let i = y; i < y + length; i++) {
+                    if (this.gameField[x][i] !== 0) {
+                        hasPlace = false;
+                        break;
+                    }
+                }
+            }
+        } else {
+            if (x + length > 10) {
+                hasPlace = false;
+            } else {
+                for (let i = x; i < x + length; i++) {
+                    if (this.gameField[i][y] !== 0) {
+                        hasPlace = false;
+                        break;
+                    }
+                }
+            }
+        }
+        if (hasPlace) {
+            const currentShip = new Ship(length);
+            if (direction === 'horizontal') {
+                for (let i = y; i < y + length; i++) {
+                    this.gameField[x][i] = currentShip;
+                }
+            }
+        } else {
+            for (let i = x; i < x + length; i++) {
+                this.gameField[i][y] = currentShip;
+            }
+        }
+    }
 }
